2c071bf2d3a0f28fb5a3ef2b9f9497e4a16bbbea
ASpoonyBard3 <Samsonf21@gmail.com>
2017-09-30 17:07:22 +0100

    finished with list of colleciton initialiser

--- WorkingWithCollections/Program.cs
+++ WorkingWithCollections/Program.cs
@@ -24,18 +24,20 @@ namespace WorkingWithCollections
             b1.Title = "Microsoft .NET XML Web Services";
             b1.ISBN = "0-000-000000-0";
 
+            /*
             ArrayList myArrayList = new ArrayList();
             myArrayList.Add(Car1);
             myArrayList.Add(Car2);
             myArrayList.Add(b1);
             myArrayList.Remove(b1);
 
+
             foreach (Car car in myArrayList)
             {
                 Console.WriteLine(car.Make);
                 Console.ReadLine();
             }
-            /*
+            
             //List<T>
             List<Car> myList = new List<Car>();
             myList.Add(Car1);
@@ -56,10 +58,16 @@ namespace WorkingWithCollections
             myDictionary.Add(Car2.VIN, Car2);
 
             Console.WriteLine(myDictionary["B2"]);
-            */
 
             Car car1 = new Car() { Make = "BMW", Model = "750li", VIN = "C3" };
             Car car2 = new Car() { Make = "Toyota", Model = "4Runner", VIN = "D3" };
+*/
+            List<Car> myList = new List<Car>()
+            {
+                new Car { Make = "Oldsmobile", Model = "Cutlas Supreme", VIN = "E5"},
+                new Car {Make = "Nissan", Model = "altima", VIN = "F6"}
+            };
+
 
 
             Console.ReadLine();
