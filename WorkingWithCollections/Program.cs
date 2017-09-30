3226c53a749245f30f9782a34cae2d7657402aa6
ASpoonyBard3 <Samsonf21@gmail.com>
2017-09-30 16:44:48 +0100

    added lists and list with generic data type prevention

--- WorkingWithCollections/Program.cs
+++ WorkingWithCollections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,13 +11,48 @@ namespace WorkingWithCollections
     {
         static void Main(string[] args)
         {
+            Car Car1 = new Car();
+            Car1.Make = "Oldsmobile";
+            Car1.Model = "Cutlas supreme";
 
+            Car Car2 = new Car();
+            Car2.Make = "Geo";
+            Car2.Model = "Prism";
+
+            Book b1 = new Book();
+            b1.Author = "Robert Tabor";
+            b1.Title = "Microsoft .NET XML Web Services";
+            b1.ISBN = "0-000-000000-0";
+            /*
+            ArrayList myArrayList = new ArrayList();
+            myArrayList.Add(Car1);
+            myArrayList.Add(Car2);
+            myArrayList.Add(b1);
+            myArrayList.Remove(b1);
+
+            foreach (Car car in myArrayList)
+            {
+                Console.WriteLine(car.Make);
+                Console.ReadLine();
+            }
+            */
+            //List<T>
+            List<Car> myList = new List<Car>();
+            myList.Add(Car1);
+            myList.Add(Car2);
+            //myList.Add(b1);
+
+            foreach (Car car in myList)
+            {
+                Console.WriteLine(car.Model);
+                Console.ReadLine();
+            }
 
         }
 
         class Car
         {
-            public string Make {get; set;}
+            public string Make { get; set; }
             public string Model { get; set; }
         }
 
@@ -26,4 +62,5 @@ namespace WorkingWithCollections
             public string Author { get; set; }
             public string ISBN { get; set; }
         }
+    }
 }
