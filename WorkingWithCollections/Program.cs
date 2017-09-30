3ec696a114d1d8a72bffc7002b2246c6e39a52f5
ASpoonyBard3 <Samsonf21@gmail.com>
2017-09-30 15:54:50 +0100

    added first two test classes for learning lists and dictionaries

--- WorkingWithCollections/Program.cs
+++ WorkingWithCollections/Program.cs
@@ -13,5 +13,17 @@ namespace WorkingWithCollections
 
 
         }
-    }
+
+        class Car
+        {
+            public string Make {get; set;}
+            public string Model { get; set; }
+        }
+
+        class Book
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public string ISBN { get; set; }
+        }
 }
